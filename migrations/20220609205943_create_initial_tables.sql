324e471332d07f565601942d078830587e7bf4dc
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-07-12 16:44:24 -0400

    add connection to read data from seaport smart contracts

--- migrations/20220609205943_create_initial_tables.sql
+++ migrations/20220609205943_create_initial_tables.sql
@@ -20,71 +20,17 @@ CREATE TABLE addresses
     address CHAR(40) PRIMARY KEY
 );
 
--- Create fees table
-CREATE TABLE fees
-(
-    id           BIGSERIAL PRIMARY KEY,
-    address      CHAR(40) REFERENCES addresses,
-    -- Basis points can never exceed 327%, but they shouldn't need to.
-    basis_points SMALLINT
-);
-
--- Create considerations table
-CREATE TABLE considerations
-(
-    id                     BIGSERIAL PRIMARY KEY,
-    -- This is the ItemType enum
-    item_type              SMALLINT,
-    token                  CHAR(40) REFERENCES addresses,
-    identifier_or_criteria uint_256,
-    start_amount           uint_256,
-    end_amount             uint_256,
-    recipient              CHAR(40) REFERENCES addresses
-);
-
--- Create offers table
-CREATE TABLE offers
-(
-    id                     BIGSERIAL PRIMARY KEY,
-    item_type              SMALLINT,
-    token                  CHAR(40) REFERENCES addresses,
-    identifier_or_criteria uint_256,
-    start_amount           uint_256,
-    end_amount             uint_256
-);
-
--- Create order components table
-CREATE TABLE order_components
-(
-    id            BIGSERIAL PRIMARY KEY,
-    offerer       CHAR(40) REFERENCES addresses,
-    zone          CHAR(40) REFERENCES addresses,
-    offer         BIGSERIAL REFERENCES offers,
-    consideration BIGSERIAL REFERENCES considerations,
-    start_time    uint_256,
-    end_time      uint_256,
-    zone_hash     CHAR(64),
-    salt          uint_256,
-    conduit_key   CHAR(64),
-    counter       uint_256
-);
-
 CREATE TABLE orders
 (
     id               BIGSERIAL PRIMARY KEY,
-    created_date     uint_256,
-    closing_date     uint_256                      NULL,
-    listing_time     uint_256,
-    expiration_time  uint_256,
+    created_date     BIGINT,
+    closing_date     BIGINT                      NULL,
+    listing_time     BIGINT,
+    expiration_time  BIGINT,
     order_hash       CHAR(64)                      NULL,
-    protocol_data    BIGSERIAL REFERENCES order_components,
     protocol_address CHAR(40) REFERENCES addresses NULL,
     maker            CHAR(40) REFERENCES addresses,
     taker            CHAR(40) REFERENCES addresses NULL,
-    -- TODO(Nail down the unit here)
-    current_price    TEXT,
-    maker_fees       BIGSERIAL REFERENCES fees,
-    taker_fees       BIGSERIAL REFERENCES fees,
     side             SMALLINT,
     order_type       SMALLINT,
     canceled         BOOLEAN,
--- src/routes/create_offer.rs
+++ src/routes/create_offer.rs
@@ -1,21 +1,27 @@
-use crate::seaport::Order;
+use crate::seaport::{Order, OrderComponents, Seaport};
 use actix_web::{post, web, HttpResponse};
+use ethers::abi::Address;
+use ethers::providers::{Http, Provider};
 use sqlx::PgPool;
 
 #[post("/offers")]
 #[tracing::instrument(
 name = "Adding a new offer",
-skip(offer, pool),
+skip(offer, pool, seaport),
 fields(
 offerer = %offer.parameters.offerer,
 )
 )]
-async fn create_offer(offer: web::Json<Order>, pool: web::Data<PgPool>) -> HttpResponse {
+async fn create_offer(
+    offer: web::Json<Order>,
+    pool: web::Data<PgPool>,
+    seaport: web::Data<Seaport<Http>>,
+) -> HttpResponse {
     let new_offer = match offer.0.try_into() {
         Ok(offer) => offer,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    if insert_offer(&pool, &new_offer).await.is_err() {
+    if insert_offer(&pool, &new_offer, &seaport).await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
     HttpResponse::Ok().finish()
@@ -23,11 +29,16 @@ async fn create_offer(offer: web::Json<Order>, pool: web::Data<PgPool>) -> HttpR
 
 #[tracing::instrument(
     name = "Saving new offer details in the database",
-    skip(new_offer, pool)
+    skip(new_offer, pool, seaport)
 )]
-pub async fn insert_offer(pool: &PgPool, new_offer: &Order) -> Result<(), sqlx::Error> {
+pub async fn insert_offer(
+    pool: &PgPool,
+    new_offer: &Order,
+    seaport: &Seaport<Http>,
+) -> Result<(), sqlx::Error> {
     // TODO(Implement queries)
     // The order model in the database differs significantly from the contract order parameters
     // Hashes are used, which must be updated from indexed events
+    //let order_hash =
     Ok(())
 }
