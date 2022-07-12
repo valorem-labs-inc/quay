af6e748bb345ef7f3da6242b2eb6538d80f378ea
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-07-12 12:28:17 -0400

    update offer test body

--- src/routes/retrieve_listings.rs
+++ src/routes/retrieve_listings.rs
@@ -1,6 +1,5 @@
 use actix_web::{get, HttpResponse, Responder};
 
-// TODO(Implement model)
 // TODO(Implement queries)
 // Cleanroom rewrite of: https://docs.opensea.io/v2.0/reference/retrieve-listings
 
