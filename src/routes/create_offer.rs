7958ad14c9f7de191b46b813144d5798b4d7671b
Alcibiades <89996683+0xAlcibiades@users.noreply.github.com>
2022-07-22 16:48:26 -0400

    docs, pipeline fix, build fix

--- src/routes/create_offer.rs
+++ src/routes/create_offer.rs
@@ -1,9 +1,48 @@
-use crate::seaport::Order;
-use actix_web::{post, web, HttpResponse, Responder};
-
-// TODO(Implement queries)
+use crate::seaport::{Order, OrderComponents, Seaport};
+use actix_web::{post, web, HttpResponse};
+use ethers::prelude::*;
+use sqlx::PgPool;
 
 #[post("/offers")]
-async fn create_offer(_offer: web::Json<Order>) -> impl Responder {
-    HttpResponse::Ok()
+#[tracing::instrument(
+name = "Adding a new offer",
+skip(offer, pool, seaport),
+fields(
+offerer = %offer.parameters.offerer,
+)
+)]
+async fn create_offer(
+    offer: web::Json<Order>,
+    pool: web::Data<PgPool>,
+    seaport: web::Data<Seaport<Provider<ethers::providers::Http>>>,
+) -> HttpResponse {
+    if insert_offer(&pool, &offer, &seaport).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+    HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(
+    name = "Saving new offer details in the database",
+    skip(new_offer, _pool, seaport)
+)]
+pub async fn insert_offer(
+    _pool: &PgPool,
+    new_offer: &Order,
+    seaport: &Seaport<Provider<Http>>,
+) -> Result<(), sqlx::Error> {
+    // TODO(Implement queries)
+    // The order model in the database differs significantly from the contract order parameters
+    // Hashes are used, which must be updated from indexed events
+    let order_hash = seaport
+        .get_order_hash(OrderComponents::from_parameters(seaport, &new_offer.parameters).await)
+        .call()
+        .await
+        .expect("failed to calculate hash");
+    let _order_status = seaport
+        .get_order_status(order_hash)
+        .call()
+        .await
+        .expect("failed to get order status");
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,16 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use ethers::prelude::*;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use std::str::FromStr;
+use std::sync::Arc;
 use tracing_actix_web::TracingLogger;
 
 use crate::routes::*;
 
 use crate::configuration::{DatabaseSettings, Settings};
+use crate::seaport::Seaport;
 use sqlx::postgres::PgPoolOptions;
 
 // A new type to hold the newly built server and its port
@@ -27,7 +31,9 @@ impl Application {
         );
         let listener = TcpListener::bind(&address)?;
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, connection_pool)?;
+        let provider: Provider<Http> =
+            Provider::new(Http::from_str(configuration.rpc.uri.as_str()).unwrap());
+        let server = run(listener, connection_pool, provider)?;
 
         // We "save" the bound port in one of `Application`'s fields
         Ok(Self { port, server })
@@ -57,8 +63,17 @@ pub struct ApplicationBaseUrl(pub String);
 #[derive(Debug)]
 pub struct RPCUri(pub String);
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    rpc: Provider<Http>,
+) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
+    let provider = Arc::new(rpc);
+    let seaport = web::Data::new(Seaport::new(
+        H160::from_str("0x00000000006c3852cbEf3e08E8dF289169EdE581").unwrap(),
+        provider,
+    ));
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
@@ -68,6 +83,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .service(create_listing)
             .service(create_offer)
             .app_data(db_pool.clone())
+            .app_data(seaport.clone())
     })
     .listen(listener)?
     .run();
