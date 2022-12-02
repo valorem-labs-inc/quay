3ba30c84caef2ce845312b3d95a3d801a25111fc
p7m <perpetuum7@protonmail.com>
2022-12-02 20:55:59 +0100

    hmac secret isn't needed if there's no session

--- src/configuration.rs
+++ src/configuration.rs
@@ -49,7 +49,6 @@ pub struct ApplicationSettings {
     pub port: u16,
     pub host: String,
     pub base_url: String,
-    pub hmac_secret: Secret<String>,
 }
 
 #[derive(serde::Deserialize, Clone)]
