32453847f374f83cf2a6e2f1aef93575b126a99d
p7m <perpetuum7@protonmail.com>
2022-12-03 13:59:12 +0100

    style migration file

--- migrations/20220609205943_create_initial_tables.sql
+++ migrations/20220609205943_create_initial_tables.sql
@@ -15,17 +15,24 @@ CREATE TABLE networks
 CREATE TABLE orders
 (
     hash TEXT PRIMARY KEY,
+
     offerer citext REFERENCES addresses(address) NOT NULL,
+
     zone citext REFERENCES addresses(address) NOT NULL,
     zone_hash TEXT NOT NULL,
+
     start_time BIGINT NOT NULL,
     end_time BIGINT NOT NULL,
+    
     order_type INT NOT NULL,
     total_original_consideration_items INT NOT NULL,
     salt TEXT NOT NULL,
+
     counter BIGINT NOT NULL,
     conduit_key TEXT NOT NULL,
+
     signature TEXT NOT NULL,
+
     cancelled BOOLEAN NOT NULL DEFAULT FALSE,
     finalized BOOLEAN NOT NULL DEFAULT FALSE,
     marked_invalid BOOLEAN NOT NULL DEFAULT FALSE
@@ -33,11 +40,13 @@ CREATE TABLE orders
 
 CREATE TABLE offers
 (
-    position INT NOT NULL,
     "order" TEXT REFERENCES orders(hash) NOT NULL,
+    position INT NOT NULL,
     item_type INT NOT NULL,
+
     token citext REFERENCES addresses(address) NOT NULL,
     identifier_or_criteria TEXT NOT NULL,
+
     start_amount TEXT NOT NULL,
     end_amount TEXT NOT NULL,
 
@@ -46,13 +55,16 @@ CREATE TABLE offers
 
 CREATE TABLE considerations
 (
-    position INT NOT NULL,
     "order" TEXT REFERENCES orders(hash) NOT NULL,
+    position INT NOT NULL,
     item_type INT NOT NULL,
+
     token citext REFERENCES addresses(address) NOT NULL,
     identifier_or_criteria TEXT NOT NULL,
+
     start_amount TEXT NOT NULL,
     end_amount TEXT NOT NULL,
+
     recipient citext REFERENCES addresses(address) NOT NULL,
 
     PRIMARY KEY("order", position)
