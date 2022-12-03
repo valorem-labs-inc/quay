1f5d4ba082cbf9654b0d6619bb06e7fdb2dc3137
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-12-03 17:32:10 -0500

    switch sqlx to tokio

--- src/startup.rs
+++ src/startup.rs
@@ -25,7 +25,7 @@ use crate::services::*;
 
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
     PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
+        .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.with_db())
 }
 
