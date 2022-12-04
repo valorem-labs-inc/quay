271a1a53907a1a3bdda8365dd0103b7b2b1d21c3
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-12-04 15:35:15 -0500

    fix CI

--- src/startup.rs
+++ src/startup.rs
@@ -33,7 +33,7 @@ use crate::{bindings::Seaport, state::AppState};
 
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
     PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.with_db())
 }
 
