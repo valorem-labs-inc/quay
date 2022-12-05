e221ede38bf07565e987d68cd68b1c7014c30efb
p7m <perpetuum7@protonmail.com>
2022-12-05 12:29:44 +0100

    implement helpers in legacy listing creation

--- src/routes/seaport/legacy/seaport_legacy_create_listing.rs
+++ src/routes/seaport/legacy/seaport_legacy_create_listing.rs
@@ -7,8 +7,11 @@ use ethers::{abi::AbiEncode, prelude::*};
 use http::StatusCode;
 use sqlx::PgPool;
 
-use crate::structs::OrderInput;
-use crate::{bindings::seaport::Seaport, database::save_address};
+use crate::{
+    bindings::seaport::Seaport,
+    database::{save_address, save_consideration, save_offer},
+};
+use crate::{database::save_order, structs::OrderInput};
 
 pub async fn seaport_legacy_create_listing(
     State(db_pool): State<PgPool>,
@@ -57,44 +60,13 @@ pub async fn insert_listing(
             tracing::error!("Failed to execute query: {:?}", e);
             e
         })?;
-    sqlx::query!(
-        r#"
-            INSERT INTO orders (
-                hash,
-                offerer,
-                zone,
-                zone_hash,
-                start_time,
-                end_time,
-                order_type,
-                total_original_consideration_items,
-                counter,
-                salt,
-                conduit_key,
-                signature
-            )
-                VALUES ($1, $2::TEXT::citext, $3::TEXT::citext, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-                ON CONFLICT (hash) DO NOTHING;
-        "#,
-        order_hash.encode_hex(),
-        new_listing.parameters.offerer.encode_hex(),
-        new_listing.parameters.zone.encode_hex(),
-        new_listing.parameters.zone_hash.encode_hex(),
-        new_listing.parameters.start_time.as_u64() as i64,
-        new_listing.parameters.end_time.as_u64() as i64,
-        new_listing.parameters.order_type as i32,
-        new_listing.parameters.total_original_consideration_items as i32,
-        new_listing.parameters.nonce as i64,
-        new_listing.parameters.salt.to_string(),
-        new_listing.parameters.conduit_key.encode_hex(),
-        new_listing.signature.to_string(),
-    )
-    .execute(&mut tx)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    save_order(order_hash, new_listing)
+        .execute(&mut tx)
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+        })?;
     let mut position = 0;
     for offer in &new_listing.parameters.offer {
         save_address(offer.token)
@@ -105,35 +77,13 @@ pub async fn insert_listing(
                 e
             })?;
 
-        // Insert offer item
-        sqlx::query!(
-            r#"
-                INSERT INTO offers (
-                    position,
-                    "order",
-                    item_type,
-                    token,
-                    identifier_or_criteria,
-                    start_amount,
-                    end_amount
-                )
-                    VALUES ($1, $2, $3, $4::TEXT::citext, $5, $6, $7)
-                    ON CONFLICT ("order", position) DO NOTHING;
-            "#,
-            position,
-            order_hash.encode_hex(),
-            offer.item_type as i32,
-            offer.token.encode_hex(),
-            offer.identifier_or_criteria.encode_hex(),
-            offer.start_amount.encode_hex(),
-            offer.end_amount.encode_hex()
-        )
-        .execute(&mut tx)
-        .await
-        .map_err(|e| {
-            tracing::error!("Failed to execute query: {:?}", e);
-            e
-        })?;
+        save_offer(order_hash, position, offer)
+            .execute(&mut tx)
+            .await
+            .map_err(|e| {
+                tracing::error!("Failed to execute query: {:?}", e);
+                e
+            })?;
         position += 1;
     }
     position = 0;
@@ -153,36 +103,13 @@ pub async fn insert_listing(
                 e
             })?;
 
-        sqlx::query!(
-            r#"
-                INSERT INTO considerations (
-                    position,
-                    "order",
-                    item_type,
-                    token,
-                    identifier_or_criteria,
-                    start_amount,
-                    end_amount,
-                    recipient
-                )
-                    VALUES ($1, $2, $3, $4::TEXT::citext, $5, $6, $7, $8::TEXT::citext)
-                    ON CONFLICT ("order", position) DO NOTHING;
-            "#,
-            position,
-            order_hash.encode_hex(),
-            consideration.item_type as i32,
-            consideration.token.encode_hex(),
-            consideration.identifier_or_criteria.encode_hex(),
-            consideration.start_amount.encode_hex(),
-            consideration.end_amount.encode_hex(),
-            consideration.recipient.encode_hex()
-        )
-        .execute(&mut tx)
-        .await
-        .map_err(|e| {
-            tracing::error!("Failed to execute query: {:?}", e);
-            e
-        })?;
+        save_consideration(order_hash, position, consideration)
+            .execute(&mut tx)
+            .await
+            .map_err(|e| {
+                tracing::error!("Failed to execute query: {:?}", e);
+                e
+            })?;
         position += 1;
     }
     tx.commit().await.map_err(|e| {
