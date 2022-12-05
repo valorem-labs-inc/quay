1a6b1cb99122eccd98b7fcd51ba21cd035a8971b
p7m <perpetuum7@protonmail.com>
2022-12-05 11:23:26 +0100

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 pub mod bindings;
 pub mod configuration;
+pub mod database;
 pub mod indexer;
-pub mod rfq {
-    #![allow(clippy::derive_partial_eq_without_eq)]
-    tonic::include_proto!("quay");
-}
 pub mod middleware;
 pub mod redis_pool;
 pub mod routes;
@@ -14,4 +11,8 @@ pub mod state;
 pub mod structs;
 pub mod telemetry;
 pub mod utils;
-pub mod database;
+
+pub mod rfq {
+    #![allow(clippy::derive_partial_eq_without_eq)]
+    tonic::include_proto!("quay");
+}
