a09a04f5b5cc577e6c32dabdd72b8c5a19bfd549
p7m <perpetuum7@protonmail.com>
2022-12-05 11:28:40 +0100

    register metric storage for db

---
+++ src/telemetry/metrics/database.rs
@@ -0,0 +1,24 @@
+use std::time::Duration;
+
+use prometheus::{HistogramVec, IntCounterVec};
+use prometheus_metric_storage::{MetricStorage, StorageRegistry};
+
+use super::get_metric_storage_registry;
+
+#[derive(MetricStorage, Clone, Debug)]
+#[metric(subsystem = "database")]
+pub struct DatabaseMetrics {
+    /// Execution times of DB queries.
+    #[metric(labels("type"))]
+    database_queries: HistogramVec,
+}
+
+impl DatabaseMetrics {
+    pub fn inst(registry: &StorageRegistry) -> Result<&Self, prometheus::Error> {
+        DatabaseMetrics::instance(registry)
+    }
+
+    pub fn get() -> &'static Self {
+        DatabaseMetrics::instance(get_metric_storage_registry()).unwrap()
+    }
+}
