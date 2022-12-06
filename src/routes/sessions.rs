54bf217d1fcf767175973a869774684b29a66cbf
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-12-05 19:47:28 -0500

    handle ts error

--- src/routes/sessions.rs
+++ src/routes/sessions.rs
@@ -7,6 +7,10 @@ const NONCE_KEY: &'static str = "nonce";
 const EXPIRATION_TIME_KEY: &'static str = "expirationTime";
 const USER_ADDRESS_KEY: &'static str = "userAddress";
 
+fn unix_timestamp() -> Result<u64, anyhow::Error> {
+    Ok(SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs())
+}
+
 //#[tracing::instrument(name = "Getting an EIP-4361 nonce for session", skip(session))]
 pub async fn get_nonce(mut session: WritableSession) -> impl IntoResponse {
     let nonce = siwe::generate_nonce();
@@ -15,13 +19,11 @@ pub async fn get_nonce(mut session: WritableSession) -> impl IntoResponse {
         Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
     }
     // Make sure we don't inherit a dirty settion expiry
-    match session.insert(
-        EXPIRATION_TIME_KEY,
-        SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs(),
-    ) {
+    let ts = match unix_timestamp() {
+        Ok(ts) => ts,
+        Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
+    };
+    match session.insert(EXPIRATION_TIME_KEY, ts) {
         Ok(_) => {}
         Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
     }
