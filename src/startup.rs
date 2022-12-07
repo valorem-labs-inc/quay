bcc89b5795af52e4a22cc9d2f0f679850fc9586a
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-12-06 20:22:47 -0500

    stub out gRPC services

--- src/startup.rs
+++ src/startup.rs
@@ -27,7 +27,7 @@ use tower_http::trace::TraceLayer;
 
 use crate::middleware::{track_prometheus_metrics, RequestIdLayer};
 use crate::redis_pool::RedisConnectionManager;
-use crate::rfq::trader_server::TraderServer;
+use crate::rfq::rfq_server::RfqServer;
 use crate::routes::*;
 use crate::services::*;
 use crate::{bindings::Seaport, state::AppState};
@@ -91,7 +91,7 @@ pub fn run(
     let grpc = Server::builder()
         .layer(RequestIdLayer)
         .layer(TraceLayer::new_for_http().make_span_with(TowerMakeSpanWithConstantId))
-        .add_service(TraderServer::new(TraderRFQService::default()))
+        .add_service(RfqServer::new(RFQService::default()))
         .into_service()
         .map_response(|r| r.map(axum::body::boxed))
         .boxed_clone();
