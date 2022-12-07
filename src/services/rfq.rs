5c451f626f3d1da407e96ec7a0a1ef2ed2732b50
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-12-06 21:28:19 -0500

    rehash rfq

--- src/services/rfq.rs
+++ src/services/rfq.rs
@@ -1,5 +1,5 @@
 use crate::rfq::rfq_server::Rfq;
-use crate::rfq::{QuoteResponse, ValoremQuoteRequest};
+use crate::rfq::{QuoteResponse, QuoteRequest};
 use std::pin::Pin;
 use tokio::sync::mpsc::channel;
 use tonic::codegen::futures_core::Stream;
@@ -13,17 +13,17 @@ impl Rfq for RFQService {
     type WebTakerStream = Pin<Box<dyn Stream<Item = Result<QuoteResponse, Status>,> + Send + 'static>>;
 
     async fn web_taker(&self,
-    _request: Request<ValoremQuoteRequest>,) -> Result<Response<Self::WebTakerStream>, Status> {
+    _request: Request<QuoteRequest>,) -> Result<Response<Self::WebTakerStream>, Status> {
         todo!("Implement me")
     }
 
     type TakerStream = Pin<Box<dyn Stream<Item = Result<QuoteResponse, Status>,> + Send + 'static>>;
 
-    async fn taker(&self, _request: Request<Streaming<ValoremQuoteRequest>>) -> Result<Response<Self::TakerStream>, Status> {
+    async fn taker(&self, _request: Request<Streaming<QuoteRequest>>) -> Result<Response<Self::TakerStream>, Status> {
         todo!("Implement me")
     }
 
-    type MakerStream = Pin<Box<dyn Stream<Item = Result<ValoremQuoteRequest, Status>,> + Send + 'static>>;
+    type MakerStream = Pin<Box<dyn Stream<Item = Result<QuoteRequest, Status>,> + Send + 'static>>;
 
     async fn maker(&self, _request: Request<Streaming<QuoteResponse>>) -> Result<Response<Self::MakerStream>, Status> {
         todo!("Implement me")
