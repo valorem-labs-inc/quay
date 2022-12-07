2bd6c33834fb605e71120d3dc9e6aed5396deaf6
Enrique Ortiz <hi@enriqueortiz.dev>
2022-12-07 15:19:30 -0400

    feat: keep abstracting gossip node funcs

--- src/bin/gossip.rs
+++ src/bin/gossip.rs
@@ -1,71 +1,27 @@
 use anyhow::Result;
-use futures::StreamExt;
-use libp2p::{
-    gossipsub::{Gossipsub, GossipsubEvent},
-    identity, mdns,
-    swarm::{NetworkBehaviour, SwarmEvent},
-};
+use libp2p::identity;
 use quay::{
     configuration::get_configuration,
     gossip::QuayGossipNode,
     telemetry::{get_subscriber, init_subscriber},
 };
-use tracing::log::info;
-
-#[derive(NetworkBehaviour)]
-struct SeaportGossipBehaviour {
-    pub gossipsub: Gossipsub,
-    pub mdns: mdns::tokio::Behaviour,
-}
+use tracing::error;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let subscriber = get_subscriber("quay-gossip".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    info!("Starting Quay Gossip Client");
-
-    let _config = get_configuration().expect("Could not read config");
+    let config = get_configuration().expect("Could not read config");
 
     let keypair = identity::Keypair::generate_ed25519();
-    let mut node = QuayGossipNode::new(keypair)?;
 
-    node.swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+    let node = QuayGossipNode::new(keypair)?;
 
-    loop {
-        tokio::select! {
-                event = node.swarm.select_next_some() => {
-                    match event {
-                        SwarmEvent::NewListenAddr { address, .. } => {
-                            info!("Listening on {address:?}");
-                        }
-                        SwarmEvent::Behaviour(quay::gossip::SeaportGossipBehaviourEvent::Gossipsub(GossipsubEvent::Message{ message, .. })) => {
-                            info!(
-                                    "Received: '{:?}' from {:?}",
-                                    String::from_utf8_lossy(&message.data),
-                                    message.source
-                                );
-                        }
-                        SwarmEvent::Behaviour(quay::gossip::SeaportGossipBehaviourEvent::Mdns(event)) => {
-                            match event {
-                                mdns::Event::Discovered(list) => {
-                                    for (peer, _) in list {
-                                        node.swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer);
-                                    }
-                                }
-                                mdns::Event::Expired(list) => {
-                                    for (peer, _) in list {
-                                        if !node.swarm.behaviour().mdns.has_node(&peer) {
-                                            node.swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        _ => {}
-
-                }
-            }
-        }
+    if let Err(e) = node.run(config.gossip).await {
+        error!("Unhandled node error. Exiting");
+        panic!("{}", e);
     }
+
+    Ok(())
 }
--- src/gossip.rs
+++ src/gossip.rs
@@ -5,23 +5,49 @@ use std::{
 };
 
 use anyhow::Result;
+use futures::StreamExt;
 use libp2p::{
     core::upgrade,
     gossipsub::{
-        self, Gossipsub, GossipsubMessage, MessageAuthenticity, MessageId, ValidationMode,
+        self,
+        error::{PublishError, SubscriptionError},
+        Gossipsub, GossipsubEvent, GossipsubMessage, IdentTopic as Topic, MessageAuthenticity,
+        MessageId, ValidationMode,
     },
+    identify,
     identity::Keypair,
-    mdns, mplex, noise,
-    swarm::NetworkBehaviour,
+    mdns, mplex, noise, ping,
+    swarm::{NetworkBehaviour, SwarmEvent},
     tcp, PeerId, Swarm, Transport,
 };
+use tracing::info;
 
-pub const PROTOCOL_ID: &str = "/seaport/0.0.1";
+use crate::configuration::GossipNodeSettings;
+
+pub const PROTOCOL_ID: &str = "seaport/0.0.1";
 
 #[derive(NetworkBehaviour)]
 pub struct SeaportGossipBehaviour {
     pub gossipsub: Gossipsub,
+    pub identify: identify::Behaviour,
+    pub ping: ping::Behaviour,
     pub mdns: mdns::tokio::Behaviour,
+}
+
+impl SeaportGossipBehaviour {
+    /// Publish data over the gossip network.
+    pub fn publish(
+        &mut self,
+        topic: Topic,
+        data: impl Into<Vec<u8>>,
+    ) -> Result<MessageId, PublishError> {
+        self.gossipsub.publish(topic, data)
+    }
+
+    /// Subscribe to a gossip topic.
+    pub fn subscribe(&mut self, topic: &Topic) -> Result<bool, SubscriptionError> {
+        self.gossipsub.subscribe(topic)
+    }
 }
 
 pub struct QuayGossipNode {
@@ -57,12 +83,23 @@ impl QuayGossipNode {
             .build()
             .expect("Config should be valid");
 
-        let gossipsub = Gossipsub::new(MessageAuthenticity::Signed(keypair), gossipsub_config)
-            .expect("Correct configuration");
+        let gossipsub = Gossipsub::new(
+            MessageAuthenticity::Signed(keypair.clone()),
+            gossipsub_config,
+        )
+        .expect("Correct configuration");
 
         let swarm = {
             let mdns = mdns::tokio::Behaviour::new(mdns::Config::default())?;
-            let behaviour = SeaportGossipBehaviour { gossipsub, mdns };
+            let behaviour = SeaportGossipBehaviour {
+                gossipsub,
+                mdns,
+                ping: Default::default(),
+                identify: identify::Behaviour::new(identify::Config::new(
+                    PROTOCOL_ID.into(),
+                    keypair.public(),
+                )),
+            };
             Swarm::with_tokio_executor(transport, behaviour, peer_id)
         };
 
@@ -70,5 +107,49 @@ impl QuayGossipNode {
             swarm,
             local_peer_id: peer_id,
         })
+    }
+
+    pub async fn run(mut self, config: GossipNodeSettings) -> Result<()> {
+        info!("Starting Quay Gossip Client");
+        self.swarm
+            .listen_on(format!("/ip4/{}/tcp/{}", config.host_name, config.port).parse()?)?;
+
+        loop {
+            tokio::select! {
+                    event = self.swarm.select_next_some() => {
+                        match event {
+                            SwarmEvent::NewListenAddr { address, .. } => {
+                                info!("Listening on {address:?}");
+                            }
+                            SwarmEvent::Behaviour(SeaportGossipBehaviourEvent::Gossipsub(GossipsubEvent::Message{ message, .. })) => {
+                                info!(
+                                        "Received: '{:?}' from {:?}",
+                                        String::from_utf8_lossy(&message.data),
+                                        message.source
+                                    );
+                            }
+                            SwarmEvent::Behaviour(SeaportGossipBehaviourEvent::Mdns(event)) => {
+                                match event {
+                                    mdns::Event::Discovered(list) => {
+                                        for (peer, _) in list {
+                                            self.swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer);
+                                        }
+                                    }
+                                    mdns::Event::Expired(list) => {
+                                        for (peer, _) in list {
+                                            if !self.swarm.behaviour().mdns.has_node(&peer) {
+                                                self.swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            SwarmEvent::Behaviour(SeaportGossipBehaviourEvent::Identify(_)) => {},
+                            _ => {}
+
+                    }
+                }
+            }
+        }
     }
 }
